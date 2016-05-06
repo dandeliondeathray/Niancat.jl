4c684ad12866bd0b0895866addca8c1dec254073
Erik Edin <erikedin.name@gmail.com>
2016-05-06 10:37:13 +0200

    implement rest of responses

--- src/responder.jl
+++ src/responder.jl
@@ -10,9 +10,6 @@ end
 send(r::Responder, channelId::ChannelId, text::UTF8String) =
     send_event(r.client, OutgoingMessageEvent(text, channelId))
 
-respond(r::Responder, response::SolutionNotificationResponse) =
-    send(r, r.main_channel, utf8("$(response.name) löste nian: $(response.hash)"))
-
 macro response(response_type::Symbol, s::Expr)
     quote
         $(esc(:respond))(r::Responder, response::$response_type) =
@@ -22,3 +19,34 @@ end
 
 @response IncorrectSolutionResponse utf8("Ordet $(response.word) finns inte med i SAOL.")
 @response CorrectSolutionResponse   utf8("Ordet $(response.word) är korrekt!")
+@response NoPuzzleSetResponse utf8("Dagens nia är inte satt!")
+@response InvalidPuzzleResponse utf8("$(response.puzzle) är inte giltig!")
+
+respond(r::Responder, response::SolutionNotificationResponse) =
+    send(r, r.main_channel, utf8("$(response.name) löste nian: $(response.hash)"))
+
+respond(r::Responder, response::UnknownUserSolutionResponse) =
+    send(r, r.main_channel, utf8("<@$(response.user)> löste nian, men är en okänd användare"))
+
+
+function respond(r::Responder, response::GetPuzzleResponse)
+    s = ""
+    if response.solutions == 1
+        s = utf8("$(response.puzzle)")
+    else
+        s = utf8("$(response.puzzle)\nDen har $(response.solutions) lösningar.")
+    end
+    send(r, response.channel, s)
+end
+
+function respond(r::Responder, response::SetPuzzleResponse)
+    s = ""
+    if response.solutions == 1
+        s = utf8("Dagens nia är satt till $(response.puzzle)")
+    else
+        s = utf8("Dagens nia är satt till. $(response.puzzle)\nDen har $(response.solutions) lösningar.")
+    end
+    send(r, response.channel, s)
+end
+
+respond(r::Responder, ::IgnoredEventResponse) = nothing
--- test/responder_test.jl
+++ test/responder_test.jl
@@ -5,8 +5,8 @@ import DandelionSlack: OutgoingEvent, OutgoingMessageEvent, AbstractRTMClient, s
 type TestEvent
     channel::ChannelId
     has_texts::Vector{UTF8String}
-
-    TestEvent(channelId::ChannelId, xs...) = new(channelId, [xs...])
+    has_not_texts::Vector{UTF8String}
+    TestEvent(channelId::ChannelId, xs...; has_not=[]) = new(channelId, [xs...], has_not)
 end
 
 type ResponderTest
@@ -24,12 +24,52 @@ responder_tests = [
     ResponderTest(
         "Incorrect solution response to user",
         IncorrectSolutionResponse(ChannelId("D0"), Word("FOO")),
-        [TestEvent(ChannelId("D0"), "FOO")]),
+        [TestEvent(ChannelId("D0"), "FOO", "inte")]),
 
     ResponderTest(
         "Correct solution response to user",
         CorrectSolutionResponse(ChannelId("D0"), Word("FOO")),
-        [TestEvent(ChannelId("D0"), "FOO")]),
+        [TestEvent(ChannelId("D0"), "FOO", "korrekt")]),
+
+    ResponderTest(
+        "Unknown user",
+        UnknownUserSolutionResponse(UserId("U0")),
+        [TestEvent(main_channel_id, "<@U0>", "känd")]),
+
+    ResponderTest(
+        "Get puzzle, many solutions",
+        GetPuzzleResponse(ChannelId("C0"), Puzzle("PUZZLE"), 17),
+        [TestEvent(ChannelId("C0"), "PUZZLE", "17")]),
+
+    ResponderTest(
+        "Get puzzle, one solution",
+        GetPuzzleResponse(ChannelId("C0"), Puzzle("PUZZLE"), 1),
+        [TestEvent(ChannelId("C0"), "PUZZLE"; has_not=["1"])]),
+
+    ResponderTest(
+        "No puzzle set",
+        NoPuzzleSetResponse(ChannelId("C0")),
+        [TestEvent(ChannelId("C0"), "inte", "satt")]),
+
+    ResponderTest(
+        "Set puzzle response, many solutions",
+        SetPuzzleResponse(ChannelId("C0"), Puzzle("PUZZLE"), 17),
+        [TestEvent(ChannelId("C0"), "PUZZLE", "17")]),
+
+    ResponderTest(
+        "Set puzzle response, one solution",
+        SetPuzzleResponse(ChannelId("C0"), Puzzle("PUZZLE"), 1),
+        [TestEvent(ChannelId("C0"), "PUZZLE"; has_not=["1"])]),
+
+    ResponderTest(
+        "Invalid puzzle",
+        InvalidPuzzleResponse(ChannelId("C0"), Puzzle("PUZZLE")),
+        [TestEvent(ChannelId("C0"), "PUZZLE")]),
+
+    ResponderTest(
+        "Ignore events",
+        IgnoredEventResponse(ChannelId("C0"), utf8("Some text")),
+        [])
 ]
 
 type FakeRTMClient <: AbstractRTMClient
@@ -62,6 +102,10 @@ facts("Responder") do
 
                 for text in test_event.has_texts
                     @fact contains(message.text, text) --> true
+                end
+
+                for text in test_event.has_not_texts
+                    @fact contains(message.text, text) --> false
                 end
             end
         end
